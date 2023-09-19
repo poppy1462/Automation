0867b9232b67249f0671cc1142959b9c46f00049
Anastasia Tskhay <anastasiya.tskhay@gmail.com>
2023-09-18 20:28:36 -0500

    chapter14assignment2.py was added

---
+++ chapter14assignment2.py
@@ -0,0 +1,17 @@
+#Exercise 1
+my_string="Test Automation"
+#Slice the string in such way that it will print only "Test"
+word_test=my_string[:5]
+print(word_test)
+
+#Exercise 2
+my_string="Test Automation"
+#Slice the string in such a way that it will print only "Automation"
+second_word=my_string[-10:]
+print(second_word)
+
+#Exercise 3
+my_list=[10,50,20,25,30]
+#Slice the second and third elements (50 and 20) in the list
+sliced_list=my_list[1:3]
+print(sliced_list)
