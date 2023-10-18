c0f75f5b5a6f2db06ea9fef655b19ee02b2d3675
Anastasia Tskhay <anastasiya.tskhay@gmail.com>
2023-10-17 22:22:09 -0500

    chapter16-2.py was added

---
+++ chapter16-2.py
@@ -0,0 +1,32 @@
+# Exercise 1
+def show_employee(name, salary=50000):
+    print(name,salary)
+
+show_employee(name="Anastasia",salary="20000")
+show_employee("Sarah",30000)
+show_employee("John")
+
+# Exercise 2
+# Define shout_echo
+def shout_echo(word1, echo=1):
+    """Concatenate echo copies of word1 and three
+     exclamation marks at the end of the string."""
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = word1*echo
+
+    # Concatenate '!!!' to echo_word: shout_word
+    shout_word = echo_word + '!!!'
+
+    # Return shout_word
+    return shout_word
+
+# Call shout_echo() with "Hey": no_echo
+no_echo = shout_echo("Hey")
+
+# Call shout_echo() with "Hey" and echo=5: with_echo
+with_echo =shout_echo("Hey", 5)
+
+# Print no_echo and with_echo
+print(no_echo)
+print(with_echo)
