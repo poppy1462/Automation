be039ff1217360722c18e1a58679b7dd06c43eb9
Anastasia Tskhay <anastasiya.tskhay@gmail.com>
2023-11-12 16:33:31 -0600

    Chapter 18 Assignment 1

---
+++ conditions.py
@@ -0,0 +1,19 @@
+import random
+
+my_num = random.randint(0, 50)
+print(my_num)
+
+
+def guess_numbers():
+    for i in range(7):
+        user_number = input("Enter the number:\n")
+        if int(user_number) < my_num:
+            print("The value is too low")
+            if int(user_number) > my_num:
+                print("The number is too high")
+        else:
+            print("You guessed correctly")
+            break
+
+
+guess_numbers()
