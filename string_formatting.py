aae76b06284eed3f9a9ec81796468f43a8c3e3f0
Anastasia Tskhay <anastasiya.tskhay@gmail.com>
2023-11-12 19:22:32 -0600

    Chapter 18 Assignment 2 Part 1

---
+++ string_formatting.py
@@ -0,0 +1,5 @@
+user_input = input("Write any word (or 'stop' to exit):\n")
+
+while user_input != 'stop':
+    print(user_input.lower())
+    user_input = input("Write any word (or 'stop' to exit):\n")
